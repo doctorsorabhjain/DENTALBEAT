da8db112ddae8b7a85386fcb55b837c2d2ba263b
doctorsorabhjain <doctorsorabhjain@gmail.com>
2026-03-15 23:54:57 +0530

    Dental Beat 2026 Issue2

---
+++ issues/2026/issue2/config.js
@@ -0,0 +1,8 @@
+window.FLIPBOOK_CONFIG={
+  "totalSlides": 10,
+  "startPage": 1,
+  "pageFolder": "pages/",
+  "hotspotFile": "hotspots.json",
+  "galleryFile": "gallery.json",
+  "issuePath": "issue2"
+}
