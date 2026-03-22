27d800fd5ec737381d2fa0c4b57f709e054f8219
doctorsorabhjain <doctorsorabhjain@gmail.com>
2026-03-22 17:53:35 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,24 @@
+const CACHE_NAME = "flipbook-v1"
+
+self.addEventListener("install", e => {
+  self.skipWaiting()
+})
+
+self.addEventListener("activate", e => {
+  clients.claim()
+})
+
 self.addEventListener("fetch", event => {
 
   const url = new URL(event.request.url)
 
-  // ❌ NEVER cache index.html or query-based requests
+  // ❌ Do NOT cache main page or issue-based URLs
   if (url.pathname === "/" || url.search.includes("issue=")) {
     event.respondWith(fetch(event.request))
     return
   }
 
-  // ✅ cache only static assets
+  // ✅ Cache other files (images, js, css)
   event.respondWith(
     caches.match(event.request).then(res => {
       return res || fetch(event.request)
